06d7c81a8c41cc10d89b39000571c8b75525719f
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-08-18 05:40:19 +0530

    completed

--- Routers/fileRouter.js
+++ Routers/fileRouter.js
@@ -5,7 +5,5 @@ const router = express.Router();
 const fileController = require("../Controller/fileController");
 
 router.route("/").post(fileController.uploads, fileController.postfile);
-
 router.route("/visualize").get(fileController.visualize);
-
 module.exports = router;
